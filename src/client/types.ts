734f9386941adabc5017a5dc76124f9d14a5ac8e
Gyuri Ordody <gordody@gmail.com>
2026-03-02 14:55:02 -0800

    move client code into client folder

--- src/client/types.ts
+++
@@ -1,2 +0,0 @@
-// Re-export types from server for use in frontend client
-export type { GameSession, Player, MovePayload, MoveResult, GameConfig, ClientToServerEvents, ServerToClientEvents } from '../../server/src/types';
